1103cbcd468f9135502c96d7547ca1fbd6c8a18d
Ben Hall <ben@benhall.me.uk>
2019-10-03 16:22:57 +0100

    Typo

--- placeholders/foreground2.sh
+++ placeholders/foreground2.sh
@@ -1,1 +1,1 @@
-echo "The URL to access Port 80 is [[HOST_SxeUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
+echo "The URL to access Port 80 is [[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
