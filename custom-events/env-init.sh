3e05031be45a348ae7880f8d6be6e14adf9fd995
Ben Hall <ben@benhall.me.uk>
2019-10-30 11:28:32 +0100

    Change URL

--- custom-events/env-init.sh
+++ custom-events/env-init.sh
@@ -1,4 +1,4 @@
-curl -L https://www.dropbox.com/s/m30l752trdcgvu8/site.tar.gz?dl=0 > ~/site.tar.gz
+curl -L https://github.com/katacoda/scenario-examples/blob/master/custom-events/site/site.tar.gz?raw=true > ~/site.tar.gz
 tar -xvzf ~/site.tar.gz -C /home/scrapbook/tutorial
 cd /home/scrapbook/tutorial
 npm install
