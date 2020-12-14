5a654bb3670c5b0e9b674d8a93eb58138f10db5a
Gabriela S. Soria <soria.gaby@gmail.com>
2020-12-14 11:10:14 -0800

    KHD-223 | example scenario to install VS Code extensions
    Signed-off-by: Gabriela S. Soria <soria.gaby@gmail.com>

---
+++ vscode-extensions/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+cd /tmp && apt install bsdtar -y
+curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/humao/vsextensions/rest-client/0.24.3/vspackage | bsdtar -xvf - extension
+mv extension /opt/.katacodacode/extensions/humao.rest-client-0.24.3
