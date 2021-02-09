7ef65068daff837e2b37a4c7094755e6dbaf5fb3
Ben Hall <ben@benhall.me.uk>
2021-02-09 11:46:58 +0000

    Extend example to use step content too

--- automatedtest/.cypress/example_spec.js
+++ automatedtest/.cypress/example_spec.js
@@ -3,7 +3,7 @@ describe('My First Test', () => {
     cy.startScenario() // This would automatically go to the scenario being tested. Without this the test will fail.
   });
 
-  it('finds the content "Start Scenario"', () => {
+  it('finds the "Start Scenario" button', () => {
     cy.contains('Start Scenario');
   });
 
@@ -11,5 +11,19 @@ describe('My First Test', () => {
     cy.terminalType("uname");
 
     cy.terminalShouldContain('Linux');
-  })
+  });
+
+  it('can verify contents of a step', () => {
+    cy.stepShouldBeVisible(1);
+
+    cy.stepHasText("This is an example scenario that will be tested using the Cypress integration");
+  });
+
+  it('can go to the next step and verify contents', () => {
+    cy.goToStep(2);
+
+    cy.stepShouldBeVisible(2);
+
+    cy.stepHasText('This will be the second step.');
+  });
 })
