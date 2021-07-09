5c80a1a1c676fc37eaa8412cfd6e74a024d195b7
Gabriela S. Soria <soria.gaby@gmail.com>
2021-07-09 11:45:41 -0400

    Unify commands to install bsdtar
    Signed-off-by: Gabriela S. Soria <soria.gaby@gmail.com>

--- vscode-extensions/setup.sh
+++ vscode-extensions/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-cd /tmp && apt update && apt install bsdtar -y
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - # add the latest key
+cd /tmp && apt-get update -y && apt install libarchive-tools -y # install bsdtar
 curl -L https://marketplace.visualstudio.com/_apis/public/gallery/publishers/humao/vsextensions/rest-client/0.24.3/vspackage | bsdtar -xvf - extension
 mv extension /opt/.katacodacode/extensions/humao.rest-client-0.24.3
